ea58332cd5b89c720539e43e8139eb57a1637c8b
valentinotto <32565438+valentinotto@users.noreply.github.com>
2018-01-09 13:48:47 +0100

    .

--- ServerTest.js
+++ ServerTest.js
@@ -1,11 +1,12 @@
 "use strict";
-const Http = require("http");
+exports.__esModule = true;
+var Http = require("http");
 var ServerTest;
 (function (ServerTest) {
-    let port = process.env.PORT;
+    var port = process.env.PORT;
     if (port == undefined)
         port = 8100;
-    let server = Http.createServer();
+    var server = Http.createServer();
     server.addListener("listening", handleListen);
     server.addListener("request", handleRequest);
     server.listen(port);
@@ -15,7 +16,7 @@ var ServerTest;
     function handleRequest(_request, _response) {
         console.log("Ich höre Stimmen!!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.write("Ich höre<br> Stimmen!!");
+        _response.write("Ich höre<br> Stimmen  !!!");
         _response.end();
     }
 })(ServerTest || (ServerTest = {}));
--- ServerTest.ts
+++ ServerTest.ts
@@ -16,7 +16,7 @@ namespace ServerTest {
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("Ich höre Stimmen!!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.write("Ich höre<br> Stimmen!!");
+        _response.write("Ich höre<br> Stimmen  !!!");
         _response.end();
     }
 }
