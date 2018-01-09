cec6660d77dac9ad1df8504d76554a5fdeb6c714
valentinotto <32565438+valentinotto@users.noreply.github.com>
2018-01-09 12:41:22 +0100

    .

--- test.ts
+++ test.ts
@@ -1,1 +1,23 @@
-console.log("Test");
+import * as http from "http";
+
+
+
+namespace ServerTest {
+
+  let server: Http.Server = Http.createServer();
+  server.addListener("Listening", handleListen);
+  server.addListener("request", handleRequest);
+  server.listen(8100);
+
+
+function handleListen() :void {
+  console.log("Listening")
+}
+
+  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse) :void {
+    console.log("Ich höre stimmen");
+    _response.setHeader("content-type", "text/html ; charset= utf-8");
+    _response.write("Ich höre Stimmen!")
+    _response.end();
+  }
+}
