1fa0a366d0c0a50f845ef37a81278a4452184719
valentinotto <32565438+valentinotto@users.noreply.github.com>
2018-01-18 18:38:57 +0100

    .

--- ServerTest.js
+++ ServerTest.js
@@ -1,22 +1,46 @@
 "use strict";
 exports.__esModule = true;
+// Node-Http-Modul importieren
 var Http = require("http");
+// Node-Url-Modul importieren
+var Url = require("url");
 var ServerTest;
 (function (ServerTest) {
+    // Port vom Process-Objekt erfragen
     var port = process.env.PORT;
+    // Port nicht definiert -> lokale Maschine, Port selbst definieren
     if (port == undefined)
         port = 8100;
+    // Server-Objekt kreieren
     var server = Http.createServer();
+    // Event-Handler installieren
     server.addListener("listening", handleListen);
     server.addListener("request", handleRequest);
+    // Auf dem Port horchen
     server.listen(port);
+    // Listening-Event: Rückmeldung wenn horchen läuft
     function handleListen() {
-        console.log("Server listening");
+        console.log("Server listening on port " + port);
     }
+    // Request-Event: Verarbeiten der Request und erstellen der Response
     function handleRequest(_request, _response) {
         console.log("Ich höre Stimmen!!");
+        // Header: Antwort kommt im HTML-Format mit uft-8
         _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.write("Ich höre<br> Stimmen  !!!");
+        // Header: ?
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        // Response-Body
+        _response.write("Ich höre Stimmen!!<br>");
+        _response.write("Port: " + port + "<br>");
+        _response.write("Method: " + _request.method + "<br>");
+        _response.write("Url: " + _request.url + "<br>");
+        _response.write("Headers: " + _request.headers + "<br>");
+        // ?
+        var query = Url.parse(_request.url, true).query;
+        // ?
+        for (var key in query)
+            _response.write(key + ": " + query[key]);
+        // Antwort abschließen und abschicken
         _response.end();
     }
 })(ServerTest || (ServerTest = {}));
