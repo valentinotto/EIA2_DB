b3203b227afb92b8beb50160709b7784a79e0aed
valentinotto <32565438+valentinotto@users.noreply.github.com>
2018-01-24 11:49:53 +0100

    .

---
+++ Database.js
@@ -0,0 +1,46 @@
+"use strict";
+exports.__esModule = true;
+/**
+ * Simple database insertion and query for MongoDB
+ * @author: Jirka Dell'Oro-Friedl
+ */
+var Mongo = require("mongodb");
+console.log("Database starting");
+var databaseURL = "mongodb://localhost:27017";
+var databaseName = "Test";
+var db;
+var students;
+if (process.env.NODE_ENV == "production") {
+    //    databaseURL = "mongodb://username:password@hostname:port/database";
+    databaseURL = "mongodb://testuser:testpassword@ds129532.mlab.com:29532/eia2";
+    databaseName = "eia2";
+}
+Mongo.MongoClient.connect(databaseURL, handleConnect);
+function handleConnect(_e, _db) {
+    if (_e)
+        console.log("Unable to connect to database, error: ", _e);
+    else {
+        console.log("Connected to database!");
+        db = _db.db(databaseName);
+        students = db.collection("students");
+    }
+}
+function insert(_doc) {
+    students.insertOne(_doc, handleInsert);
+}
+exports.insert = insert;
+function handleInsert(_e) {
+    console.log("Database insertion returned -> " + _e);
+}
+function findAll(_callback) {
+    var cursor = students.find();
+    cursor.toArray(prepareAnswer);
+    function prepareAnswer(_e, studentArray) {
+        if (_e)
+            _callback("Error" + _e);
+        else
+            _callback(JSON.stringify(studentArray));
+    }
+}
+exports.findAll = findAll;
+//# sourceMappingURL=Database.js.map
